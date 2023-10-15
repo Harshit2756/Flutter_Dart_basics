5c553b422b9a51a04f4c0c6b1d31f7c5d8eba0f2
Harshit khandelwal <khandelwalharshit431@gmail.com>
2023-10-15 14:48:20 +0530

    Weather_App(flutter_works,Listview,futureBuilder)

--- Flutter_Project/weather_app/lib/main.dart
+++ Flutter_Project/weather_app/lib/main.dart
@@ -1,125 +1,51 @@
 import 'package:flutter/material.dart';
+import './weather_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
+//* How Flutter UI is rendered on different platforms
+// . It paints the UI on a canvas provided by the platform i.e. it does't translate into Native UI.
+// . The canvas is a rectangular area on the screen.
+// . It is painted/renedered by its own rendering engine.i.e. Skia graphics which is not changint to Impeller.
+
+// *  Flutter Behind The Scenes, 3 Trees & BuildContext
+// . Widget Tree: It is a tree of widgets that are used to render the UI.
+// . Element Tree:It is a reflection of widget tree and is responsible for managing the lifecycle of the widgets.
+
+// - So whenever we make any changes to the widget tree, flutter performs a process called Reconciliation or diffing to identify the changes and these changes are only rebuilt using render object tree.
+// - Reconciliation is the process of comparing the current widget tree with the previous widget tree and determining the difference between them .
+// - This process is also called diffing.
+
+// . Render Object Tree: It is a tree of render objects that are responsible for painting the UI on the canvas.
+// . Render Object is a widget it has three types:
+// 1.Leaf Render Object: It is a render object that does not have any child.
+// Eg: Container, Text, Image, etc.
+// 2.Single Child Render Object: It is a render object that has only one child.
+// Eg: Center, Align, ,etc.
+// 3.Multi Child Render Object: It is a render object that has multiple children.
+// Eg: Column,Row, Stack, ListView, etc.
+
+// . BuildContext is a element and element is a reflection of widget.
+// . BuildContext is used to locate the widget in the widget tree with the help of element tree.
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      title: 'Weather App',
+      debugShowCheckedModeBanner: false,
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      // . copyWith() is a method that returns a copy of the current theme with the specified changes.
+      theme: ThemeData.dark(useMaterial3: true).copyWith(
+        // scaffoldBackgroundColor: Colors.blueGrey.shade800,
+        appBarTheme: const AppBarTheme(
+          // backgroundColor: Colors.blueGrey.shade700,
+          centerTitle: true,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: const WeatherScreen(),
     );
   }
 }
